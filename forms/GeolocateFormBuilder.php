0e1bccafb970cab424881d5f120f3fe481288caf
shaddow <lameruza@gmail.com>
2016-10-01 16:59:27 +0200

    UPDATE Forms

--- forms/GeolocateFormBuilder.php
+++ forms/GeolocateFormBuilder.php
@@ -2,16 +2,18 @@
 
 namespace Wame\Geolocate\Forms;
 
+use Wame\DynamicObject\Forms\BaseForm;
 use Wame\DynamicObject\Forms\BaseFormBuilder;
+use Wame\Geolocate\Components\GeolocateFilterControl;
 
 class GeolocateFormBuilder extends BaseFormBuilder
 {
     /** {@inheritDoc} */
-    public function submit($form, $values)
+    public function submit(BaseForm $form, array $values)
     {
-		$geolocateFilterControl = $form->lookup(\Wame\Geolocate\Components\GeolocateFilterControl::class);
-        $geolocateFilterControl->setAddress($values['address']);
-        $geolocateFilterControl->setDistance($values['nearby']);
+		$geolocateFilterControl = $form->lookup(GeolocateFilterControl::class);
+        $geolocateFilterControl->setAddress($values['AddressContainer']['address']);
+        $geolocateFilterControl->setDistance($values['NearbyContainer']['nearby']);
     }
 
 }
