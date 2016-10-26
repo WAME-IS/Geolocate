791b0af5e91505b80cfa3422b760084933b9a8d5
shadd0w <rene.gmitter@wame.sk>
2016-10-26 10:24:54 +0200

    UPDATE autocomplete-geolocate.js

--- assets/autocomplete-geolocate.js
+++ assets/autocomplete-geolocate.js
@@ -2,60 +2,9 @@
  * https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
  */
 
-var GEOLOCATE = (function() {
-    var autocomplete;
-
-    var init = function() {
-        var elms = document.getElementsByClassName( "autocomplete" );
-
-        for(var i = 0; i < elms.length; i++) {
-            var gelocate = elms[i].getAttribute('data-autocomplete');
-
-            if (gelocate === "geolocate") {
-                var type = elms[i].getAttribute('data-type');
-
-                autocomplete = new google.maps.places.Autocomplete(
-                    (elms[i]), { 
-                        types: [type] 
-                    }
-                );
-
-                autocomplete.addListener('place_changed', fillInAddress);
-            }
-        }
-    };
-
-    var fillInAddress = function() {
-        var place = autocomplete.getPlace();
-
-        var lat = place.geometry.location.lat();
-        var lng = place.geometry.location.lng();
-
-        console.log(lat, lng);
-    };
-
-    return {
-        init: init
-    };
-}());
-
 $.fn.geoAutocomplete = function(options) {
     var settings = $.extend({
-       // plugin options
-       key: 'AIzaSyBF_gAEa4OxAK4RVo_0_HQXbbou-5EJRE8'
-    }, options );
-
-    google.load('maps', '3', {
-        other_params: 'key=' + settings.key + '&libraries=places', 
-        callback: function() {
-            GEOLOCATE.init();
-        }
-    });
-
-    return this.each(function() {
-        var elm = $(this);
-
-        var componentForm = {
+        componentForm: {
             street_number: 'long_name',
             route: 'long_name',
             locality: 'long_name',
@@ -63,31 +12,44 @@ $.fn.geoAutocomplete = function(options) {
             administrative_area_level_1: 'long_name',
             country: 'short_name',
             postal_code: 'long_name'
-        };
-
-
-
-
-//            autocomplete.addListener('place_changed', function() {});
-
-
-
-
-//            changePlace();
+        }
+    }, options);
+    
+    /**
+     * Save to data
+     * 
+     * @param {string} name
+     * @param {mixed} value
+     */
+    function saveToData(name, value) {
+        if(name) {
+            $el = $("[name='" + name + "']");
+            if($el.length) {
+                $el.val(value);
+            }
+        }
+    }
+    
+    return this.each(function() {
+        var $this = $(this);
+        
+        var type = $this.data('type');
+        
+        var autocomplete = new google.maps.places.Autocomplete($this[0], {types: [type]});
+        
+        autocomplete.addListener('place_changed', changePlace);
 
         function changePlace() {
-            var elm = $( "#google-map-api-autocomplete" ),
-                url = elm.data('url');
+            var url = $this.data('url');
             var place = autocomplete.getPlace();
             var country = '';
             var address = {};
 
             $.each(place.address_components, function(index, value) {
-                var item = $(this)[index];
-                var addressType = item.types[0];
+                var addressType = value.types[0];
 
-                if (componentForm[addressType]) {
-                    var val = item[componentForm[addressType]];
+                if (settings.componentForm[addressType]) {
+                    var val = value[settings.componentForm[addressType]];
 
                     address[addressType] = val;
 
@@ -101,12 +63,15 @@ $.fn.geoAutocomplete = function(options) {
             address['latitude'] = place.geometry.location.lat();
             address['longitude'] = place.geometry.location.lng();
 
+            saveToData($this.data('el-placeId'), address['place_id']);
+            saveToData($this.data('el-latitude'), address['latitude']);
+            saveToData($this.data('el-longitude'), address['longitude']);
+
             // save address to db
-            saveObject(url, address);
-
-//                console.log(place);
-//                console.log(address);
-
+            if($this.data('url')) {
+                saveObject(url, address);
+            }
+            
 //                var label = address.locality;
 
 //                if (address.sublocality_level_1) {
@@ -119,7 +84,6 @@ $.fn.geoAutocomplete = function(options) {
 
 //                input.hide();
 //                input.after('<div class="well well-sm">' + label + '<span class="fa fa-spinner fa-pulse btn btn-sm btn-link pull-right loader"></span></div>');
-
         }
 
         // TODO: need to implement geolocation (navigator.geolocation)
@@ -161,11 +125,11 @@ $.fn.geoAutocomplete = function(options) {
             });
         }
 
-        function googleMapsApiInputEmpty() {
-            $('#google-map-api-autocomplete').val('').show();
-            $('#' + $('#google-map-api-autocomplete').data('input')).val('');
-            $('.google-map-api-autocomplete .well').remove();
-        }
+//        function googleMapsApiInputEmpty() {
+//            $('#google-map-api-autocomplete').val('').show();
+//            $('#' + $('#google-map-api-autocomplete').data('input')).val('');
+//            $('.google-map-api-autocomplete .well').remove();
+//        }
 
 //            $('.google-map-api-autocomplete').delegate('.google-map-api-autocomplete-close', 'click', function() {
 //                googleMapsApiInputEmpty();
@@ -174,8 +138,3 @@ $.fn.geoAutocomplete = function(options) {
 //            });
    });
 };
-
-
-$(function() {
-    $( "[data-autocomplete='geolocate']" ).geoAutocomplete();
-});
